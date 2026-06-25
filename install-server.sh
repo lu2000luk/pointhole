dbabf17c28b642821b9a4529fba5c95dc59cd064
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2026-06-25 22:24:08 +0200

    opsss

--- install-server.sh
+++ install-server.sh
@@ -14,7 +14,7 @@ cd $HOME/.local/bin/
 echo "Downloading server..."
 
 # add last commit to the url to bust the cache
-curl -o pointserver https://cdn.lu2000luk.com/pointhole/server/server?commit=0975c82
+curl -o pointserver https://cdn.lu2000luk.com/pointhole/server/server?commit=3997bb3
 chmod +x pointserver
 
 echo "Server installed to $HOME/.local/bin/pointserver"
